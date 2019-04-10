03df76dbaf9ffb67a25b0fa2c9020e0e49e633b8
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2019-04-10 13:50:11 -0600

    asdf

--- plugins/CSVtoJSON/index.js
+++ plugins/CSVtoJSON/index.js
@@ -1,4 +1,4 @@
-var app 		= require('./src/app');
-var stream 		= require('./src/stream');
+var app 		= require('./plugins/CSVtoJSON/app');
+var stream 		= require('./plugins/CSVtoJSON/stream');
 app.stream 		= stream;
 module.exports 	= app;
